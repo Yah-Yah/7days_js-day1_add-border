078627ac60f6de2296eccd43c0fb5e078ca2e837
Janina Zylinska <janeczka@gmail.com>
2020-06-13 20:23:47 +0100

    Add function creating border array

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 function addBorder(array) {
-  /* ADD CODE HERE */
+
+  const initialArray = array;
+  let finalArray = [];
+  initialArray.unshift('');
+  initialArray.push('');
+
+  initialArray.forEach((element) => {
+    element = element.split("");
+    element.unshift("*");
+    // console.log(typeof element);
+    // console.log(Array.isArray(element));
+    console.log(element.length);
+
+    while (element.length<5) {
+        element.push("*");
+    };
+
+    // element.toString();
+    element = element.join("");
+
+    finalArray.push(element);
+
+    // console.log(element);
+    return finalArray;
+  })
 }
