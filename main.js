ccd1d433da4004bc9a7c61349c54b19943eb45cd
Janina Zylinska <janeczka@gmail.com>
2020-06-13 20:26:34 +0100

    Cleanup code

--- main.js
+++ main.js
@@ -8,23 +8,13 @@ function addBorder(array) {
   initialArray.forEach((element) => {
     element = element.split("");
     element.unshift("*");
-    // console.log(typeof element);
-    // console.log(Array.isArray(element));
-    console.log(element.length);
-
     while (element.length<5) {
         element.push("*");
     };
-
-    // element.toString();
     element = element.join("");
-
     finalArray.push(element);
-
-    // console.log(element);
     return finalArray;
   })
-  // console.log(finalArray);
   finalArray.forEach((element) => {
     let eachParagraph = document.createElement('p');
     eachParagraph.innerHTML = element;
